af28c0a89143b6a03d386f2dbe4196fc341b74c1
Guido <f8kibi@gmail.com>
2017-02-24 22:43:43 +0100

    Init commit

---
+++ src/main/java/foo/bar/CloudsResource.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foo.bar;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author guido
+ */
+@Path("messages")
+public class CloudsResource {
+    
+    @GET
+    public String helloWorld() {
+        return "Hello World " + System.currentTimeMillis();
+    }
+             
+}
